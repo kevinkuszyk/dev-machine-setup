2f3302976a19333080409c75c2d7a6f6967df0c2
Kevin Kuszyk <kevin@kevinkuszyk.com>
2017-10-05 10:38:26 +0100

    Add some help text to the cli

--- install.cmd
+++ install.cmd
@@ -1,3 +1,23 @@
 @echo off
 
-choco install %1.config -y
+IF "%1" == "core" choco install core.config -y && GOTO exit
+IF "%1" == "svn" choco install svn.config -y && GOTO exit
+
+:help
+
+:::
+::: Dev machine setup cli
+::: 
+::: USAGE:
+::: 
+::: install {verb}
+::: 
+::: VERBS:
+:::
+:::  - core               Install core dev tools from Chocolatey.
+:::  - svn                Install svn tools from Chocolatey.
+:::
+
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+
+:exit
